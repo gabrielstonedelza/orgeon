deee54cf4ef79fdb2a81a3d1ea4b59bf5ac32642
gabrielstonenims <gabrielstonenims@gmail.com>
2020-05-16 10:25:39 +0200

    another commit

--- blog/static/blog/js/jscode.js
+++ blog/static/blog/js/jscode.js
@@ -2,7 +2,8 @@ $(function() {
   setTimeout(function() {
     $(".alert").slideUp(3000);
   }, 5000);
- 
+
+
   $(document).on("submit", ".post-comment-form", function(event) {
     event.preventDefault();
     $.ajax({
